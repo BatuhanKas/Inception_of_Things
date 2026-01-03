4deb9bcceecfd2abab09e6411c40bd2b6288935a
Batuhan Kas <batuhannkas@gmail.com>
2026-01-03 17:52:01 +0300

    fix: architecture amd64 changed.

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -10,7 +10,7 @@ systemctl start docker
 usermod -aG docker vagrant
 
 echo "[2] Install kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl" # Download kubectl binary arm64
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" # Download kubectl binary amd64
 chmod +x kubectl
 mv kubectl /usr/local/bin/
 
