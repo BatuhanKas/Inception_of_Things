2990e5a5f93d58738c19a304daebc2afc3bcdd7c
Batuhan Kas <batuhannkas@gmail.com>
2026-01-19 15:19:19 +0300

    fix: core number changes

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -41,7 +41,7 @@ function install_kubectl()
         echo "[INFO] Installing kubectl..."
 
         curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-        chmod +x kubectl
+        chmod 777 kubectl
         $SUDO mv kubectl /usr/local/bin/
 
         echo "[INFO] kubectl installed."
@@ -52,9 +52,7 @@ function install_k3d()
 {
     if ! is_installed k3d; then
         echo "[INFO] Installing k3d..."
-
         curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
         echo "[INFO] k3d installed."
     fi
 }
